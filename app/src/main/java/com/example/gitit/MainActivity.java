d188874ded1dda02be77559cd81e0df7c92fe33a
Aras Balali Moghaddam <arasbm@gmail.com>
2019-10-04 11:20:47 -0700

    stuff

--- app/src/main/java/com/example/gitit/MainActivity.java
+++ app/src/main/java/com/example/gitit/MainActivity.java
@@ -1,13 +1,27 @@
 package com.example.gitit;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
+
+    public static final String SECRET_MESSAGE = "SECRET_MESSAGE";
+    EditText editText;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        editText = findViewById(R.id.editText);
+    }
+
+    public void sendMessage(View view) {
+        Intent intent = new Intent(getApplicationContext(), SecondActivity.class);
+        intent.putExtra(SECRET_MESSAGE, editText.getText().toString());
+        startActivity(intent);
     }
 }
